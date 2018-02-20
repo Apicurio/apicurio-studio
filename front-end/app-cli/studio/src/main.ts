06b118a15b103b6a18fc9b79aa2d89663576295d
Eric Wittmann <eric.wittmann@gmail.com>
2018-02-20 08:33:42 -0500

    Startup console messages.  Readme update.
    Signed-off-by: Eric Wittmann <eric.wittmann@gmail.com>

--- front-end/app-cli/studio/src/main.ts
+++ front-end/app-cli/studio/src/main.ts
@@ -4,7 +4,7 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
 
-function printBanner() {
+function printBanner(env) {
   console.log(`%c ___________________________________________________
   Welcome to     _                 _
      /\\         (_)               (_)
@@ -15,21 +15,23 @@ function printBanner() {
           | |
           |_|
           
- Mode: Development
+ Mode: %s
  ___________________________________________________
-`, "font-family:monospace");
+`, "font-family:monospace", env);
 }
 
 if (environment.production) {
   enableProdMode();
-  platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.log(err));
+  platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
+    printBanner("Production");
+  }).catch(err => console.log(err));
 } else {
   var keycloak = window["Keycloak"]();
   keycloak.init({onLoad: 'login-required'}).success(function (authenticated) {
     if (authenticated) {
       window['keycloak'] = keycloak;
       platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
-        printBanner();
+        printBanner("Development");
       }).catch(err => console.log(err));
     }
   }).error(function () {

c943ddc86414be4a239e6359f3810fad1e2608a7
Eric Wittmann <eric.wittmann@gmail.com>
2018-07-24 09:16:20 -0400

    added a script to deploy the front-end to a "production" (ie Wildfly) location
    Signed-off-by: Eric Wittmann <eric.wittmann@gmail.com>

---
+++ front-end/studio/deploy-prod.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+WILDFLY_DIRECTORY=$1
+
+if [ "x$WILDFLY_DIRECTORY" = "x" ]
+then
+  read -p "Wildfly Location: " WILDFLY_DIRECTORY
+fi
+
+yarn build
+rm -rf $WILDFLY_DIRECTORY/standalone/deployments/apicurio-studio.war/*.js
+rm -rf $WILDFLY_DIRECTORY/standalone/deployments/apicurio-studio.war/*.css
+rm -rf $WILDFLY_DIRECTORY/standalone/deployments/apicurio-studio.war/*.html
+rm -rf $WILDFLY_DIRECTORY/standalone/deployments/apicurio-studio.war/assets
+cp -rf dist/* $WILDFLY_DIRECTORY/standalone/deployments/apicurio-studio.war/
+rm -rf $WILDFLY_DIRECTORY/standalone/deployments/apicurio-studio.war.*
+touch $WILDFLY_DIRECTORY/standalone/deployments/apicurio-studio.war.dodeploy
+rm -rf dist

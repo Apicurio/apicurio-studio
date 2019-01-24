53cc2d4625ea2c35380d033ecae0e686e78f5774
Eric Wittmann <eric.wittmann@gmail.com>
2019-01-24 12:58:42 -0500

    disable some junit tests that cannot be automated
    Signed-off-by: Eric Wittmann <eric.wittmann@gmail.com>

--- back-end/hub-api/src/test/java/io/apicurio/hub/api/bitbucket/BitbucketSourceConnectorTest.java
+++ back-end/hub-api/src/test/java/io/apicurio/hub/api/bitbucket/BitbucketSourceConnectorTest.java
@@ -110,6 +110,7 @@ public class BitbucketSourceConnectorTest {
     }
 
     @Test
+    @Ignore
     public void testGetTeams() throws SourceConnectorException, BitbucketException {
         Collection<BitbucketTeam> teams = service.getTeams();
         Assert.assertNotNull(teams);
@@ -120,6 +121,7 @@ public class BitbucketSourceConnectorTest {
     }
 
     @Test
+    @Ignore
     public void testGetRepositories() throws SourceConnectorException, BitbucketException {
         String team = "apicurio";
         

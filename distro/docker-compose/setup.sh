5563dab3d92d63ccd2569e27f9088df3e0dde0e3
Carles Arnal <carlesarnal92@gmail.com>
2021-02-17 11:22:49 +0100

    Expand docker compose distro db choice to keycloak db

--- distro/docker-compose/setup.sh
+++ distro/docker-compose/setup.sh
@@ -25,7 +25,7 @@ fi
 KC_ROOT_DB_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c6)
 KC_DB_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c6)
 KC_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c6)
-AS_MYSQL_ROOT_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c6)
+AS_SQL_ROOT_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c6)
 AS_DB_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c6)
 
 SERVICE_CLIENT_SECRET=$(uuidgen)
@@ -35,17 +35,21 @@ sed 's/$HOST/'"$HOST_IP"'/g' $P/.env.template > $P/tmp; mv $P/tmp $P/.env
 sed 's/$KC_ROOT_DB_PASSWORD/'"$KC_ROOT_DB_PASSWORD"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
 sed 's/$KC_DB_PASSWORD/'"$KC_DB_PASSWORD"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
 sed 's/$KC_PASSWORD/'"$KC_PASSWORD"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
-sed 's/$AS_MYSQL_ROOT_PASSWORD/'"$AS_MYSQL_ROOT_PASSWORD"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
+sed 's/$AS_SQL_ROOT_PASSWORD/'"$AS_SQL_ROOT_PASSWORD"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
 sed 's/$AS_DB_PASSWORD/'"$AS_DB_PASSWORD"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
 sed 's/$SERVICE_CLIENT_SECRET/'"$SERVICE_CLIENT_SECRET"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
 
 if [ "$DB_TYPE" == "mysql" ]
 then
   sed 's/$DB_TYPE/'"mysql5"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
+  sed 's/$KC_DB_VENDOR/'"mysql"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
+  sed 's/$KC_DB_ADDR/'"jboss-keycloak-mysql"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
   sed 's/$DB_DRIVER/'"mysql"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
   sed 's/$DB_CONN_URL/'"jdbc:mysql:\\/\\/apicurio-studio-db\\/apicuriodb"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
 else
   sed 's/$DB_TYPE/'"postgresql9"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
+  sed 's/$KC_DB_VENDOR/'"postgres"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
+  sed 's/$KC_DB_ADDR/'"jboss-keycloak-postgresql"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
   sed 's/$DB_DRIVER/'"postgresql"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
   sed 's/$DB_CONN_URL/'"jdbc:postgresql:\\/\\/apicurio-studio-db\\/apicuriodb"'/g' $P/.env > $P/tmp; mv $P/tmp $P/.env
 fi
--- distro/docker-compose/start-postgresql-environment.sh
+++ distro/docker-compose/start-postgresql-environment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.keycloak.yml build
-docker-compose -f docker-compose.keycloak.yml -f docker-compose.microcks.yml -f docker-compose.apicurio.yml -f docker-compose-as-postgre.yml up
+docker-compose -f docker-compose.keycloak-postgresql.yml build
+docker-compose -f docker-compose.keycloak-postgresql.yml -f docker-compose.microcks.yml -f docker-compose.apicurio.yml -f docker-compose-as-postgre.yml up

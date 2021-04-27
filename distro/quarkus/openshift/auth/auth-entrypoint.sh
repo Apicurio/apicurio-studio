a3074c686b004b0fcb43ee460e771fa9040a588c
Carles Arnal <carlesarnal92@gmail.com>
2021-04-27 11:16:45 +0200

    Adapt quarkus deployments to new properties structure

---
+++ distro/quarkus/openshift/auth/auth-entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ $APICURIO_KEYCLOAK_USER ] && [ $APICURIO_KEYCLOAK_PASSWORD ]; then
+    /opt/jboss/keycloak/bin/add-user-keycloak.sh --user $APICURIO_KEYCLOAK_USER --password $APICURIO_KEYCLOAK_PASSWORD
+fi
+
+
+if [ "$DB_VENDOR" == "POSTGRES" ]; then
+  databaseToInstall="postgres"
+elif [ "$DB_VENDOR" == "MYSQL" ]; then
+  databaseToInstall="mysql"
+elif [ "$DB_VENDOR" == "H2" ]; then
+  databaseToInstall=""
+else
+    if (printenv | grep '^POSTGRES_' &>/dev/null); then
+      databaseToInstall="postgres"
+    elif (printenv | grep '^MYSQL_' &>/dev/null); then
+      databaseToInstall="mysql"
+    fi                                                                                                                                                                                                                           
+fi
+
+if [ "$databaseToInstall" != "" ]; then
+    echo "[KEYCLOAK DOCKER IMAGE] Using the external $databaseToInstall database"
+    /bin/sh /opt/jboss/keycloak/bin/change-database.sh $databaseToInstall
+else
+    echo "[KEYCLOAK DOCKER IMAGE] Using the embedded H2 database"
+fi
+
+
+awk "{gsub(/APICURIO_UI_URL/,\"$APICURIO_UI_URL\");}1" /tmp/apicurio-realm.json > /tmp/apicurio-realm.configured.json
+
+exec /opt/jboss/keycloak/bin/standalone.sh $@
+exit $?

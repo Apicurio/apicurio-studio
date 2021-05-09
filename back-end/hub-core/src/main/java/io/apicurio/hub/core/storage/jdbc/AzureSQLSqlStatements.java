1a055030613cf44325490716317af1a911debc4b
s0m0bv9 <surbhi.mittal@walmart.com>
2021-05-09 18:16:59 +0530

    Azure SQL support

---
+++ back-end/hub-core/src/main/java/io/apicurio/hub/core/storage/jdbc/AzureSQLSqlStatements.java
@@ -0,0 +1,9 @@
+package io.apicurio.hub.core.storage.jdbc;
+
+/**
+ * s0m0bv9 created on 09/05/21
+ * inside the package - io.apicurio.hub.core.storage.jdbc
+ */
+
+public class AzureSQLSqlStatements {
+}

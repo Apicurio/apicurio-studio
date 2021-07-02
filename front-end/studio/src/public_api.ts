2f9c5cffc0e7abed655191cd975c210d1091c017
Zoran Regvart <zregvart@apache.org>
2021-07-02 17:11:33 +0200

    fix: expose additional components as public API
    From Angular 9 if a component is indirectly exposed as public but not
    declared public the compiler reports an error like:
    
    ```
    ERROR: src/app/pages/apis/{apiId}/editor/graphql-editor.component.ts:45:14 - error NG3001: Unsupported private class GraphQLEditorComponent. This class is visible to consumers via ApicurioEditorModule -> GraphQLEditorComponent, but is not exported from the top-level library entrypoint.
    ```
    
    This adds additional exports for indirectly exposed components.

--- front-end/studio/src/public_api.ts
+++ front-end/studio/src/public_api.ts
@@ -1,6 +1,31 @@
-import {ApicurioEditorModule} from "./app/editor.module";
 import {ApiDefinition} from './app/models/api.model';
-import {ApicurioCommonComponentsModule} from "./app/common-components.module";
 import {ApiEditorComponent} from "./app/pages/apis/{apiId}/editor/editor.component";
+import {ApicurioCommonComponentsModule} from "./app/common-components.module";
+import {ApicurioEditorModule} from "./app/editor.module";
+import {AsyncApiEditorComponent} from "./app/pages/apis/{apiId}/editor/aaieditor.component";
+import {CodeEditorComponent} from "./app/components/common/code-editor.component";
+import {DivAutoHeight, TextAreaAutosize, TextBoxAutosize} from "./app/directives/autosize.directive";
+import {DropDownComponent} from "./app/components/common/drop-down.component";
+import {FormErrorMessageComponent} from "./app/components/common/form-error-message.component";
+import {GraphQLEditorComponent} from "./app/pages/apis/{apiId}/editor/graphql-editor.component";
+import {MarkdownComponent} from "./app/components/common/markdown.component";
+import {MarkdownEditorComponent} from "./app/components/common/markdown-editor.component";
+import {MarkdownSummaryComponent} from "./app/components/common/markdown-summary.component";
 
-export {ApiDefinition, ApiEditorComponent, ApicurioEditorModule, ApicurioCommonComponentsModule};
+export {
+    ApiDefinition,
+    ApiEditorComponent,
+    ApicurioCommonComponentsModule,
+    ApicurioEditorModule,
+    AsyncApiEditorComponent,
+    CodeEditorComponent,
+    DivAutoHeight,
+    DropDownComponent,
+    FormErrorMessageComponent,
+    GraphQLEditorComponent,
+    MarkdownComponent,
+    MarkdownEditorComponent,
+    MarkdownSummaryComponent,
+    TextAreaAutosize,
+    TextBoxAutosize,
+};

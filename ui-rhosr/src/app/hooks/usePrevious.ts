06f0e69f729351f426aff47b2788156c1783cfd9
Enda Phelan <ephelan@redhat.com>
2022-03-07 14:45:00 +0000

    remove unused hook

--- ui-rhosr/src/app/hooks/usePrevious.ts
+++ ui-rhosr/src/app/hooks/usePrevious.ts
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 
+// A hook to get the value of a property prior to render
 export function usePrevious<T>(value: T): T | undefined {
 	const ref = useRef<T>();
 	useEffect(() => {

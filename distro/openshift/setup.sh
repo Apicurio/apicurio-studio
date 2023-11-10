bc03aa6a8b66e45f7368e93daafdb8c2ba59d05f
Carles Arnal <carlesarnal92@gmail.com>
2023-11-10 14:53:28 +0100

    Fix openshift distribution

--- distro/openshift/setup.sh
+++ distro/openshift/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 oc process -f apicurio-image-streams-template.yml | oc apply -f -
-oc process -f apicurio-secrets-template.yml | oc apply -f -
+oc process -f apicurio-auth-deployment.yml | oc apply -f -
+oc process -f apicurio-postgresql-deployment-config-template.yml | oc apply -f -
 oc process -f apicurio-deployment-configs-template.yml | oc apply -f -
 oc process -f apicurio-services-template.yml | oc apply -f -
 oc process -f apicurio-routes-template.yml | oc apply -f -
@@ -16,9 +17,9 @@ authurl=$(oc get route apicurio-studio-auth -o go-template --template='{{.spec.h
 
 #Set Openshift route values based on fetch
 
-oc set env dc/apicurio-studio-api APICURIO_KC_AUTH_URL=http://"$authurl"/auth/realms/apicurio
+oc set env dc/apicurio-studio-api APICURIO_KC_AUTH_URL=http://"$authurl"
 
-oc set env dc/apicurio-studio-ui APICURIO_KC_AUTH_URL=http://"$authurl"/auth/realms/apicurio
+oc set env dc/apicurio-studio-ui APICURIO_KC_AUTH_URL=http://"$authurl"
 oc set env dc/apicurio-studio-ui APICURIO_UI_HUB_API_URL=http://"$apiurl"
 oc set env dc/apicurio-studio-ui APICURIO_UI_EDITING_URL=ws://"$wsurl"
 

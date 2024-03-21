95170fa185b5bcdc31e57ec3746a98484e0bfc66
Eric Wittmann <eric.wittmann@gmail.com>
2024-03-21 10:22:16 -0400

    Better UI config in the docker image.  Removed unused dexie dependency from UI.

--- ui/.docker-scripts/create-config.cjs
+++ ui/.docker-scripts/create-config.cjs
@@ -6,9 +6,11 @@ const CONFIG_OUTPUT_PATH=process.env["APICURIO_CONFIG_OUTPUT_PATH"] || "/opt/app
 
 console.info("Generating application config at:", CONFIG_OUTPUT_PATH);
 
+const APICURIO_STUDIO_API_URL=process.env["APICURIO_STUDIO_API_URL"] || "/apis/studio/v1";
+
 const CONTEXT_PATH=process.env["APICURIO_CONTEXT_PATH"] || "/";
 const NAV_PREFIX_PATH=process.env["APICURIO_NAV_PREFIX_PATH"] || "";
-const APICURIO_STUDIO_API_URL=process.env["APICURIO_STUDIO_API_URL"] || "/apis/studio/v1";
+
 const SHOW_MASTHEAD=process.env["APICURIO_SHOW_MASTHEAD"] || "true";
 const MASTHEAD_LABEL=process.env["APICURIO_MASTHEAD_LABEL"] || "APICURIO STUDIO";
 const EDITORS_URL=process.env["APICURIO_EDITORS_URL"] || "/editors/";
@@ -20,35 +22,62 @@ const AUTH_REDIRECT_URL=process.env["APICURIO_AUTH_REDIRECT_URL"] || "";
 
 // Create the config to output.
 const CONFIG = {
-    "apis": {
-        "studio": APICURIO_STUDIO_API_URL
+    apis: {
+        studio: APICURIO_STUDIO_API_URL
     },
-    "ui": {
-        "contextPath": CONTEXT_PATH,
-        "navPrefixPath": NAV_PREFIX_PATH
+    ui: {},
+    components: {
+        masthead: {},
+        editors: {},
+        nav: {}
     },
-    "components": {
-        "masthead": {
-            "show": SHOW_MASTHEAD === "true",
-            "label": MASTHEAD_LABEL
-        },
-        "editors": {
-            "url": EDITORS_URL
-        },
-        "nav": {
-            "show": false,
-            "registry": "registry-nav"
-        }
-    },
-    "auth": {
-        "type": AUTH_TYPE,
-        "options": {
-            "redirectUri": AUTH_REDIRECT_URL,
-            "clientId": AUTH_CLIENT_ID,
-            "url": AUTH_URL
-        }
-    }
+    auth: {}
 };
+
+// Configure UI elements
+if (CONTEXT_PATH) {
+    CONFIG.ui.contextPath = CONTEXT_PATH;
+}
+if (NAV_PREFIX_PATH) {
+    CONFIG.ui.navPrefixPath = NAV_PREFIX_PATH;
+}
+
+
+// Configure Masthead elements
+if (SHOW_MASTHEAD) {
+    CONFIG.components.masthead.show = SHOW_MASTHEAD;
+}
+if (MASTHEAD_LABEL) {
+    CONFIG.components.masthead.label = MASTHEAD_LABEL;
+}
+
+// Configure Editors elements
+if (EDITORS_URL) {
+    CONFIG.components.editors.url = EDITORS_URL;
+}
+
+
+// Configure auth
+if (AUTH_TYPE) {
+    CONFIG.auth.type = AUTH_TYPE;
+}
+
+if (AUTH_TYPE === "oidc") {
+    CONFIG.auth.options = {};
+    if (AUTH_URL) {
+        CONFIG.auth.options.url = AUTH_URL;
+    }
+    if (AUTH_REDIRECT_URL) {
+        CONFIG.auth.options.redirectUri = AUTH_REDIRECT_URL;
+    }
+    if (AUTH_CLIENT_ID) {
+        CONFIG.auth.options.clientId = AUTH_CLIENT_ID_URL;
+    }
+    if (AUTH_CLIENT_SCOPES) {
+        CONFIG.auth.options.scope = AUTH_CLIENT_SCOPES;
+    }
+}
+
 
 const FILE_CONTENT = `
 const ApicurioStudioConfig = ${JSON.stringify(CONFIG, null, 4)};

0f468562396d184dcb6f3a7e25f7ece85b0b0301
Eric Wittmann <eric.wittmann@gmail.com>
2024-03-21 11:37:18 -0400

    Updated the local config overrides for local UI dev.

--- ui/ui-app/configs/config-local.js
+++ ui/ui-app/configs/config-local.js
@@ -1,5 +1,10 @@
 const ApicurioStudioConfig = {
-    "apis": {
-        "studio": "http://localhost:8080/apis/studio/v1"
+    apis: {
+        studio: "http://localhost:8080/apis/studio/v1"
+    },
+    components: {
+        editors: {
+            url: "http://localhost:9011"
+        }
     }
 };

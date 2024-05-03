21ba43ca7ba5eb3a4abf7676cf081ca529c5d2b7
Eric Wittmann <eric.wittmann@gmail.com>
2024-05-03 15:22:43 -0400

    Automate publishing of documentation to website

--- docs-playbook/_build-all.sh
+++ docs-playbook/_build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Build the docker image
-docker build -t="apicurio/apicurio-studio-docs-builder" --rm .
+docker build -t="apicurio/apicurio-docs-builder" --rm .
 
 # Use the docker image to build the docs
-docker run -v $(pwd):/apicurio-docs-playbook apicurio/apicurio-studio-docs-builder:latest
+docker run -v $(pwd)/..:/repository apicurio/apicurio-docs-builder:latest

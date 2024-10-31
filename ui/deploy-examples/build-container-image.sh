f48dd903f493d6f1946867000d87d2433057db3e
Eric Wittmann <eric.wittmann@gmail.com>
2024-10-30 20:09:55 -0400

    Tweaks to the deploy examples

--- ui/deploy-examples/build-container-image.sh
+++ ui/deploy-examples/build-container-image.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-BASE_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
-echo "Building all using base directory: $BASE_DIR"
+UI_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
+echo "Building all using base directory: $UI_DIR"
 
-cd $BASE_DIR/ui
+cd $UI_DIR/ui
 npm install
 npm run clean
 npm run build
 npm run package
 
+cd $UI_DIR/ui
 docker build -t apicurio/apicurio-studio-ui:latest-snapshot .

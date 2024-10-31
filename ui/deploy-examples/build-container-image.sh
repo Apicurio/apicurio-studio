5bdbb909eb08f6f491e35b7a601a69652c9bc02b
Eric Wittmann <eric.wittmann@gmail.com>
2024-10-31 12:31:56 -0400

    Updated dependencies where possible

--- ui/deploy-examples/build-container-image.sh
+++ ui/deploy-examples/build-container-image.sh
@@ -3,11 +3,11 @@
 UI_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
 echo "Building all using base directory: $UI_DIR"
 
-cd $UI_DIR/ui
+cd $UI_DIR
 npm install
 npm run clean
 npm run build
 npm run package
 
-cd $UI_DIR/ui
+cd $UI_DIR
 docker build -t apicurio/apicurio-studio-ui:latest-snapshot .
